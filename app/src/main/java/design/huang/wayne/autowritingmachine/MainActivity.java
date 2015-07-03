5158fbc7d3a5cc50546746c3a60386dff8b22382
Wayne Huang <wayne123tw@gmail.com>
2015-07-04 05:21:09 +0800

    Develop most
    change SDK version(for 4.0 after)
    finish dev

--- app/src/main/java/design/huang/wayne/autowritingmachine/MainActivity.java
+++ app/src/main/java/design/huang/wayne/autowritingmachine/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.app.ActionBar;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,7 +24,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
